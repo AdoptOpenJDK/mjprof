9b1e992a94be4f9ac723bbf21b08f596ffae413d
unknown <yadidh@YADIDH1.emea.hpqcorp.net>
2014-03-02 12:52:33 +0200

    fixed parsing bug. period inside parameter

---
+++ src/test/java/com/performizeit/mjstack/MJStackTest.java
@@ -0,0 +1,49 @@
+package com.performizeit.mjstack;
+
+import com.performizeit.mjstack.monads.MJStep;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+
+public class MJStackTest {
+    @Test
+    public void argWhichContainsPeriod() throws Exception {
+        String []args =  {"contains/key.k,val.val/"};
+        ArrayList<MJStep> steps = MJStack.parseCommandLine(args);
+        assertNotNull(steps);
+        assertEquals(steps.get(0).getStepName(), "contains");
+        assertEquals(steps.get(0).getStepArg(0), "key.k");
+        assertEquals(steps.get(0).getStepArg(1), "val.val");
+        assertEquals(steps.get(0).getStepArgs().size(), 2);
+
+    }
+    @Test
+    public void argWhichContainsPeriodSplit() throws Exception {
+        String arg =  "contains/key.k,val.val/.list";
+        ArrayList<String> steps = MJStack.splitCommandLine(arg);
+        assertNotNull(steps);
+        assertEquals(steps.get(0), "contains/key.k,val.val/");
+        assertEquals(steps.get(1), "list");
+
+    }
+    @Test
+    public void argWhichContainsPeriod2() throws Exception {
+        String []args =  {"contains/key.k,val.,,val/.contains/stack,com.performizeit/"};
+        ArrayList<MJStep> steps = MJStack.parseCommandLine(args);
+        assertNotNull(steps);
+        assertEquals(steps.size(),2);
+        assertEquals(steps.get(0).getStepName(), "contains");
+        assertEquals(steps.get(0).getStepArg(0), "key.k");
+        assertEquals(steps.get(0).getStepArg(1), "val.,val");
+        assertEquals(steps.get(0).getStepArgs().size(), 2);
+        assertEquals(steps.get(1).getStepName(), "contains");
+        assertEquals(steps.get(1).getStepArg(0), "stack");
+        assertEquals(steps.get(1).getStepArg(1), "com.performizeit");
+        assertEquals(steps.get(1).getStepArgs().size(), 2);
+
+    }
+}
