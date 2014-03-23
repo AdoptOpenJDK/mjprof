39b1ee70ac02af9152b51ce79694b3c3f7a5c2de
anatmaor <levingeranat@gmail.com>
2014-03-23 18:41:19 +0200

    Fix JStackFilter

--- src/main/java/com/performizeit/mjstack/api/JStackFilter.java
+++ src/main/java/com/performizeit/mjstack/api/JStackFilter.java
@@ -6,7 +6,5 @@ import com.performizeit.mjstack.parser.JStackMetadataStack;
  * Created by life on 22/2/14.
  */
 public interface JStackFilter extends BasePlugin{
-    boolean filter(JStackMetadataStack stck);
-
-	String getHelpLine();
+	public boolean filter(JStackMetadataStack stck);
 }
--- src/main/java/com/performizeit/mjstack/api/JStackMapper.java
+++ src/main/java/com/performizeit/mjstack/api/JStackMapper.java
@@ -6,5 +6,5 @@ import com.performizeit.mjstack.parser.JStackMetadataStack;
  * Created by life on 23/2/14.
  */
 public interface JStackMapper extends BasePlugin{
-    JStackMetadataStack map(JStackMetadataStack stck);
+	JStackMetadataStack map(JStackMetadataStack stck);
 }
