82383a4d7209f0c2707cae1c7d9fdb61293364d1
anatmaor <levingeranat@gmail.com>
2014-03-23 10:09:13 +0200

    Add Plugin support using annotations

---
+++ src/main/java/com/performizeit/annotation/Filter.java
@@ -0,0 +1,13 @@
+package com.performizeit.annotation;
+
+import java.lang.annotation.*;
+/*
+ * Annotation for marking the class as Filter: @Filter
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Filter {
+
+}
+
+
