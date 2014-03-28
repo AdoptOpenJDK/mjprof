7760fc679cf8eca80a8dc5e87a286f6fc2b3cbcf
anatmaor <levingeranat@gmail.com>
2014-03-28 20:18:02 +0300

    Updated synopsis and handle comparator

---
+++ src/main/java/com/performizeit/mjstack/api/JStackComparator.java
@@ -0,0 +1,11 @@
+package com.performizeit.mjstack.api;
+
+import java.util.Comparator;
+
+import com.performizeit.mjstack.parser.JStackMetadataStack;
+
+public interface JStackComparator extends BasePlugin, Comparator<JStackMetadataStack>{
+
+
+
+}
