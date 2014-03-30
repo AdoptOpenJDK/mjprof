9a2ccc5acd18b88e6335cce25b7306b1803cfd56
Haim Yadid <haim@performize-it.com>
2014-03-30 23:59:38 +0300

    fix terminals and generalize them

---
+++ src/main/java/com/performizeit/mjstack/parser/JStackDumpTerminal.java
@@ -0,0 +1,34 @@
+package com.performizeit.mjstack.parser;
+
+import com.performizeit.mjstack.api.JStackFilter;
+import com.performizeit.mjstack.api.JStackMapper;
+import com.performizeit.mjstack.api.JStackTerminal;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.Comparator;
+
+/**
+ * Created by life on 30/3/14.
+ */
+public class JStackDumpTerminal extends JStackDumpBase{
+    protected  String data;
+    public JStackDump filterDump(JStackFilter filter) {
+        throw new NotImplementedException();
+    }
+    public JStackDump mapDump(JStackMapper mapper) {
+        throw new NotImplementedException();
+    }
+    public JStackDump sortDump(Comparator<JStackMetadataStack> comp) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public JStackDumpBase terminateDump(JStackTerminal terminal) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public String toString() {
+        return header+"\n\n"+data;
+    }
+}
