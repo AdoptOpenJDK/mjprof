8beba569bea62cd8cf3838f9b8269ee7b4090b0a
unknown <yadidh@YADIDH1.emea.hpqcorp.net>
2014-04-01 15:50:04 +0300

    fix batch files

--- src/main/scripts/mjs.sh
+++ src/main/scripts/mjs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-java -cp "$DIR/mjstack-1.0.jar:$DIR/libs/*  .jar:$DIR/plugins/*" com.performizeit.mjstack.MJStack $*
+java -cp "$DIR/mjstack-1.0.jar:$DIR/libs/*.jar:$DIR/plugins/*" com.performizeit.mjstack.MJStack $*
