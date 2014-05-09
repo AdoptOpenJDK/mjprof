dffdc753f2fcade1c45438763f8f9d5080e16bbf
Haim Yadid <haim@performize-it.com>
2014-05-09 04:39:33 +0300

    rename batch files to mjprof

--- src/main/scripts/mjs.bat
+++
@@ -1,2 +0,0 @@
-@echo off 
-java -cp "%~dp0/mjprof-1.0.jar;%~dp0/lib/*.jar;%~dp0/plugins/*" com.performizeit.mjstack.MJStack   %*
--- src/main/scripts/mjs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-java -cp "$DIR/mjprof-1.0.jar:$DIR/libs/*.jar:$DIR/plugins/*" com.performizeit.mjstack.MJStack $*
