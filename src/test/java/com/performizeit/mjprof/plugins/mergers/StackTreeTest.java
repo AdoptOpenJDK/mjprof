37e94284d03637654dc17e03db632292b1764577
Haim Yadid <haim@performize-it.com>
2014-11-11 23:50:46 +0100

    decimal seperator should be localized

--- src/test/java/com/performizeit/mjprof/plugins/mergers/StackTreeTest.java
+++ src/test/java/com/performizeit/mjprof/plugins/mergers/StackTreeTest.java
@@ -19,9 +19,14 @@ package com.performizeit.mjprof.plugins.mergers;
 
 import com.performizeit.mjprof.model.Profile;
 import org.junit.Test;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
 import static org.junit.Assert.assertEquals;
 
 public class StackTreeTest {
+    static char sep = getSeparator();
 	static String stck =
 
 	"       at org.apache.hadoop.hdfs.DFSUtil.<clinit>(DFSUtil.java:128)\n"
@@ -48,23 +53,23 @@ public class StackTreeTest {
 			+ "       at org.apache.hadoop.fs.FileSystem.get(FileSystem.java:316)\n"
 			+ "       at com.akkka.aaa.bbbb.rest.FileSystemFactory.provide(FileSystemFactory.java:32)\n"
 			+ "       at com.akkka.aaa.bbb.rest.FileSystemFactory.provide(FlsFactory.java:44)\n";
-	static String tree = "100.00%     [3/3]\\ at com.akkka.aaa.bbb.rest.FileSystemFactory.provide(FlsFactory.java:44)\n"
-			+ "100.00%     [3/3] \\ at com.akkka.aaa.bbbb.rest.FileSystemFactory.provide(FileSystemFactory.java:32)\n"
-			+ "100.00%     [3/3]  \\ at org.apache.hadoop.fs.FileSystem.get(FileSystem.java:316)\n"
-			+ "100.00%     [3/3]   \\ at org.apache.hadoop.fs.FileSystem$Cache.get(FileSystem.java:2289)\n"
-			+ "100.00%     [3/3]    X at org.apache.hadoop.fs.FileSystem$Cache.getInternal(FileSystem.java:2307)\n"
-			+ " 33.33%     [1/3]    |\\ at org.apache.hadoop.fs.FileSystem.access$200(FileSystem.java:87)\n"
-			+ " 33.33%     [1/3]    | \\ at org.apache.hadoop.fs.FileSystem.createFileSystem(FileSystem.java:2273)\n"
-			+ " 33.33%     [1/3]    |  \\ at org.apache.hadoop.hdfs.DistributedFileSystem.initialize(DistributedFileSystem.java:127)\n"
-			+ " 33.33%     [1/3]    |   \\ at org.apache.hadoop.hdfs.DFSClient.<init>(DFSClient.java:410)\n"
-			+ " 33.33%     [1/3]    |    \\ at org.apache.hadoop.hdfs.DFSClient.<init>(DFSClient.java:437)\n"
-			+ " 33.33%     [1/3]    |     V at org.apache.hadoop.hdfs.DFSUtil.<clinit>(DFSUtil.java:128)\n"
-			+ " 66.67%     [2/3]     \\ at org.apache.hadoop.fs.FileSystem.access$200(FileSystem.java:86)\n"
-			+ " 66.67%     [2/3]      \\ at org.apache.hadoop.fs.FileSystem.createFileSystem(FileSystem.java:2273)\n"
-			+ " 66.67%     [2/3]       \\ at org.apache.hadoop.hdfs.DistributedFileSystem.initialize(DistributedFileSystem.java:127)\n"
-			+ " 66.67%     [2/3]        \\ at org.apache.hadoop.hdfs.DFSClient.<init>(DFSClient.java:410)\n"
-			+ " 66.67%     [2/3]         \\ at org.apache.hadoop.hdfs.DFSClient.<init>(DFSClient.java:437)\n"
-			+ " 66.67%     [2/3]          V at org.apache.hadoop.hdfs.DFSUtil.<clinit>(DFSUtil.java:128)\n";
+	static String tree = "100"+sep+"00%     [3/3]\\ at com.akkka.aaa.bbb.rest.FileSystemFactory.provide(FlsFactory.java:44)\n"
+			+ "100"+sep+"00%     [3/3] \\ at com.akkka.aaa.bbbb.rest.FileSystemFactory.provide(FileSystemFactory.java:32)\n"
+			+ "100"+sep+"00%     [3/3]  \\ at org.apache.hadoop.fs.FileSystem.get(FileSystem.java:316)\n"
+			+ "100"+sep+"00%     [3/3]   \\ at org.apache.hadoop.fs.FileSystem$Cache.get(FileSystem.java:2289)\n"
+			+ "100"+sep+"00%     [3/3]    X at org.apache.hadoop.fs.FileSystem$Cache.getInternal(FileSystem.java:2307)\n"
+			+ " 33"+sep+"33%     [1/3]    |\\ at org.apache.hadoop.fs.FileSystem.access$200(FileSystem.java:87)\n"
+			+ " 33"+sep+"33%     [1/3]    | \\ at org.apache.hadoop.fs.FileSystem.createFileSystem(FileSystem.java:2273)\n"
+			+ " 33"+sep+"33%     [1/3]    |  \\ at org.apache.hadoop.hdfs.DistributedFileSystem.initialize(DistributedFileSystem.java:127)\n"
+			+ " 33"+sep+"33%     [1/3]    |   \\ at org.apache.hadoop.hdfs.DFSClient.<init>(DFSClient.java:410)\n"
+			+ " 33"+sep+"33%     [1/3]    |    \\ at org.apache.hadoop.hdfs.DFSClient.<init>(DFSClient.java:437)\n"
+			+ " 33"+sep+"33%     [1/3]    |     V at org.apache.hadoop.hdfs.DFSUtil.<clinit>(DFSUtil.java:128)\n"
+			+ " 66"+sep+"67%     [2/3]     \\ at org.apache.hadoop.fs.FileSystem.access$200(FileSystem.java:86)\n"
+			+ " 66"+sep+"67%     [2/3]      \\ at org.apache.hadoop.fs.FileSystem.createFileSystem(FileSystem.java:2273)\n"
+			+ " 66"+sep+"67%     [2/3]       \\ at org.apache.hadoop.hdfs.DistributedFileSystem.initialize(DistributedFileSystem.java:127)\n"
+			+ " 66"+sep+"67%     [2/3]        \\ at org.apache.hadoop.hdfs.DFSClient.<init>(DFSClient.java:410)\n"
+			+ " 66"+sep+"67%     [2/3]         \\ at org.apache.hadoop.hdfs.DFSClient.<init>(DFSClient.java:437)\n"
+			+ " 66"+sep+"67%     [2/3]          V at org.apache.hadoop.hdfs.DFSUtil.<clinit>(DFSUtil.java:128)\n";
 
 	@Test
 	public void testConstructor() {
@@ -78,4 +83,11 @@ public class StackTreeTest {
 		// System.out.println(st2.toString());
 		assertEquals(tree, st2.toString());
 	}
+
+    private static char getSeparator() {
+               DecimalFormat format= (DecimalFormat) DecimalFormat.getInstance();
+                DecimalFormatSymbols symbols=format.getDecimalFormatSymbols();
+                char sep=symbols.getDecimalSeparator();
+                return sep;
+    }
 }
