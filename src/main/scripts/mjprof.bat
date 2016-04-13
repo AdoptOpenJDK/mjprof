7b117925ab02ee8748e5609b977290f128a9fdee
hyadid <hyadid@outbrain.com>
2016-04-13 10:02:55 +0300

    Fix pom files structure

--- src/main/scripts/mjprof.bat
+++ src/main/scripts/mjprof.bat
@@ -1,2 +1,2 @@
 @echo off 
-java -Dmacros.configFile=%~dp0/macros.properties -cp "%JAVA_HOME%/lib/tools.jar;%~dp0/mjprof-1.0.jar;%~dp0/lib/*.jar;%~dp0/plugins/*" com.performizeit.mjprof.MJProf   %*
+java -Dmacros.configFile=%~dp0/macros.properties -cp "%JAVA_HOME%/lib/tools.jar;%~dp0/mjprof-1.0-jar-with-dependencies.jar;%~dp0/plugins/*" com.performizeit.mjprof.MJProf   %*
